c0b8fec5918e06f767808e6413110129148bdee9
kopelthep <47904193+kopelthep@users.noreply.github.com>
2025-11-23 14:07:43 -0500

    Stuffing, il me reste a faire le destuffing

--- stuffing.py
+++ stuffing.py
@@ -1,1 +1,24 @@
 #Code pour bit stuffing 
+
+def stuffing(message: str) -> str:
+    #message est u
+    
+    print("message:",message)
+    messageout =[]
+    counter = 0
+    
+
+
+    for i in message:
+        if i not in ("0", "1"):
+            raise ValueError("message doit contenir uniquement '0' et '1'")
+        messageout.append(i)
+        if i == "1":
+            counter +=1
+            if counter == 5:
+                messageout.append(0)
+                counter = 0
+        if i == "0":
+            counter = 0
+    return "".join(messageout)
+
